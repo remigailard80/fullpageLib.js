c6fccbd76b3a7ed764297a7f9512771a9edcfbbf
KIMMINSEOK <remigailard80@korea.ac.kr>
2021-09-07 20:53:48 +0900

    fix: stoires 폴더 빌드 안되게하기 + babel src 폴더 제외

--- rollup.config.js
+++ rollup.config.js
@@ -31,7 +31,7 @@ function setUpRollup({ input, output }) {
         commonjs({
           include: /.yarn/,
         }),
-        babel({ extensions, include: ['src/**/*'], runtimeHelpers: true }), // Babel을 사용 할 수 있게 해줌
+        babel({ extensions, runtimeHelpers: true }), // Babel을 사용 할 수 있게 해줌
         url(),
         svgr(),
         typescript(), // 만든 타입을 자동으로 build된 결과물에 넣어줌.
